ef56cae49d65aaf51a1c7c3bd91fd3ed34c9a8b8
Brian Martin <bmarti44@students.kennesaw.edu>
2013-07-13 10:28:13 -0400

    still working through this tutorial

--- tut.erl
+++ tut.erl
@@ -1,5 +1,5 @@
 -module(tut).
--export([double/1, fac/1, mult/2]).
+-export([double/1, fac/1, mult/2, convert/2]).
 
 fac(1) ->
 	1;
@@ -11,3 +11,8 @@ double(X) ->
 
 mult(X, Y) ->
 	X * Y.
+
+convert(M, inch) ->
+	M / 2.54;
+convert(N, centimeter) ->
+	N * 2.54.
